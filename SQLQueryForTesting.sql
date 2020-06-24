69ac5b45db3884efc3331097c6ffce18b4ec2349
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2020-06-24 14:48:50 +0200

    get available rooms

---
+++ SQLQueryForTesting.sql
@@ -0,0 +1,17 @@
+﻿
+declare @startDate date;
+declare @endDate date;
+
+set @startDate = '2020-06-25';
+set @endDate = '2020-06-26';
+
+SELECT rt.Id, rt.Title, rt.Description, rt.Price FROM dbo.Rooms r
+INNER JOIN dbo.RoomTypes rt
+ON r.RoomTypeId = rt.Id
+WHERE r.Id not in (
+select b.RoomId 
+FROM dbo.Bookings b
+WHERE (@startDate < b.StartDate and @endDate > b.EndDate)
+	or (b.StartDate <= @endDate and @endDate < b.EndDate)
+	or (b.StartDate <= @startDate and @startDate < b.EndDate))
+GROUP BY rt.Id, rt.Title, rt.Description, rt.Price
---
+++ hotelapp.Data/Repositories/BookingRepository.cs
@@ -0,0 +1,28 @@
+﻿using hotelapp.Data.Databases;
+using hotelapp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hotelapp.Data.Repositories
+{
+    public class BookingRepository
+    {
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public BookingRepository(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            return _db.LoadData<RoomTypeModel, dynamic>(
+                "dbo.spRoomTypes_GetAvailableRoomTypes",
+                new { StartDate = startDate, EndDate = endDate },
+                connectionStringName,
+                true);
+        }
+    }
+}
