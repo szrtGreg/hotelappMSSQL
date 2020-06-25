95fb711ba09160bed12de9232d9a95663da1255a
Grzegorz Mutkowski <grzegorz.mutkowski@gmail.com>
2020-06-25 18:28:17 +0200

    book guest

--- hotelapp.Data/Repositories/BookingRepository.cs
+++ hotelapp.Data/Repositories/BookingRepository.cs
@@ -2,6 +2,7 @@
 using hotelapp.Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace hotelapp.Data.Repositories
@@ -19,10 +20,49 @@ namespace hotelapp.Data.Repositories
         public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
         {
             return _db.LoadData<RoomTypeModel, dynamic>(
-                "dbo.spRoomTypes_GetAvailableRoomTypes",
-                new { StartDate = startDate, EndDate = endDate },
-                connectionStringName,
-                true);
+                            "dbo.spRoomTypes_GetAvailableRoomTypes",
+                            new { StartDate = startDate, EndDate = endDate },
+                            connectionStringName,
+                            true);
+        }
+
+        public void BookGuest(string firstName,
+                              string lastName,
+                              DateTime startDate,
+                              DateTime endDate,
+                              int roomTypeId)
+        {
+            GuestsModel guest = _db.LoadData<GuestsModel, dynamic>(
+                            "dbo.spGuests_Insert",
+                            new { },
+                            connectionStringName,
+                            true).First();
+
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>(
+                            "select * from dbo.RoomTypes where Id = @Id",
+                            new { Id = roomTypeId },
+                            connectionStringName,
+                            false).First();
+
+            TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
+
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRoomTypes_GetAvailableRoomTypes",
+                            new { startDate, endDate, roomTypeId },
+                            connectionStringName,
+                            true);
+
+            _db.SaveData("spBookings_Insert",
+                         new
+                         {
+                             roomId = availableRooms.First().Id,
+                             guestId = guest.Id,
+                             startDate = startDate,
+                             endDate = endDate,
+                             totalCost = timeStaying.Days * roomType.Price
+                         },
+                         connectionStringName,
+                         true);
+
         }
     }
 }
---
+++ hotelappDB/dbo/Stored Procedures/spRooms_GetAvailableRooms.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[spRooms_GetAvailableRooms]
+	@startDate date,
+	@endDate date,
+	@roomTypeId int
+AS
+begin
+	set nocount on;
+
+	SELECT r.* FROM dbo.Rooms r
+		INNER JOIN dbo.RoomTypes rt
+		ON RoomTypeId = rt.Id
+		WHERE r.RoomTypeId = @roomTypeId 
+		and r.Id not in (
+		SELECT b.RoomId FROM dbo.Bookings b
+		WHERE @startDate < b.StartDate and @endDate > b.EndDate
+		 or (b.StartDate <= @endDate and @endDate < b.EndDate)
+		 or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	)
+
+end
